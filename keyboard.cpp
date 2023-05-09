77b19137b7b810852db8054b54b69cde8721fdb7
Linus Oleander <linus.oleander@clark.de>
2023-05-09 16:30:37 +0200

    Improve OTA

--- keyboard.cpp
+++ keyboard.cpp
@@ -48,6 +48,8 @@ void handleDisconnectedClicks(ID id) {
   }
 }
 
+bool fn = false;
+
 void clickHandler(void *p) {
   ID id = POINTER(p);
 
@@ -58,36 +60,74 @@ void clickHandler(void *p) {
 
   switch (id) {
   case BUTTON_A_A_BLACK:
+    if (fn) {
+      keyboard.print('C');
+      PRINTF("[0x%x] [Click] [Fn] Restore map\n", id);
+    } else {
+      sendFnKeyPress('R');
+      PRINTF("[0x%x] [Click] Play music\n", id);
+    }
+    break;
+  case BUTTON_A_B_BLUE:
+    if (fn) {
+      keyboard.write(KEY_ZOOM_IN);
+      PRINTF("[0x%x] [Click] [Fn] Zoom In\n", id);
+    } else {
+      keyboard.write(KEY_MEDIA_PREVIOUS_TRACK);
+      PRINTF("[0x%x] [Click] Previous Track\n", id);
+    }
+    break;
+  case BUTTON_A_C_BLACK:
+    if (fn) {
+      keyboard.write(KEY_ZOOM_OUT);
+      PRINTF("[0x%x] [Click] [Fn] Zoom Out\n", id);
+    } else {
+      keyboard.write(KEY_MEDIA_VOLUME_DOWN);
+      PRINTF("[0x%x] [Click] Volume Down\n", id);
+    }
+    break;
+  case BUTTON_A_D_RED:
     keyboard.write(KEY_MEDIA_PLAY_PAUSE);
     PRINTF("[0x%x] [Click] Play/Pause\n", id);
     break;
-  case BUTTON_A_B_BLUE:
-    keyboard.write(KEY_MEDIA_NEXT_TRACK);
-    PRINTF("[0x%x] [Click] Next Track\n", id);
-    break;
-  case BUTTON_A_C_BLACK:
-    keyboard.write(KEY_MEDIA_VOLUME_UP);
-    PRINTF("[0x%x] [Click] Volume Up\n", id);
-    break;
-  case BUTTON_A_D_RED:
-    keyboard.write(KEY_MEDIA_EJECT);
-    PRINTF("[0x%x] [Click] Toggle keyboard\n", id);
-    break;
   case BUTTON_B_A_BLACK:
-    sendFnKeyPress('H');
-    PRINTF("[0x%x] [Click] Help\n", id);
+    fn = !fn;
+
+    if (fn) {
+      // sendFnKeyPress('1');
+      PRINTF("[0x%x] [Click] Fn ON\n", id);
+    } else {
+      // sendFnKeyPress('0');
+      PRINTF("[0x%x] [Click] Fn OFF\n", id);
+    }
+
     break;
   case BUTTON_B_B_BLUE:
-    keyboard.write(KEY_ZOOM_OUT);
-    PRINTF("[0x%x] [Click] Zoom out\n", id);
+    if (fn) {
+      sendFnKeyPress('A');
+      PRINTF("[0x%x] [Click] [Fn] Random Music\n", id);
+    } else {
+      keyboard.write(KEY_MEDIA_NEXT_TRACK);
+      PRINTF("[0x%x] [Click] Next Track\n", id);
+    }
     break;
   case BUTTON_B_C_BLACK:
-    sendFnKeyPress('R');
-    PRINTF("[0x%x] [Click] Play music\n", id);
+    if (fn) {
+      sendFnKeyPress('B');
+      PRINTF("[0x%x] [Click] [Fn] Random Podcast\n", id);
+    } else {
+      keyboard.write(KEY_MEDIA_VOLUME_UP);
+      PRINTF("[0x%x] [Click] Volume UP\n", id);
+    }
     break;
   case BUTTON_B_D_RED:
-    sendFnKeyPress('N');
-    PRINTF("[0x%x] [Click] Toggle noise cancelling\n", id);
+    if (fn) {
+      sendFnKeyPress('E');
+      PRINTF("[0x%x] [Click] [Fn] Navigation\n", id);
+    } else {
+      sendFnKeyPress('N');
+      PRINTF("[0x%x] [Click] Toggle noise cancelling\n", id);
+    }
     break;
   default:
     sendNOPKey(id);
@@ -101,15 +141,14 @@ void doubleClickHandler(void *p) {
 
   switch (id) {
   case BUTTON_A_A_BLACK:
-    sendNOPKey(id);
+    sendFnKeyPress('D');
+    PRINTF("[0x%x] [Double] Podcast\n", id);
     break;
   case BUTTON_A_B_BLUE:
-    keyboard.write(KEY_MEDIA_PREVIOUS_TRACK);
-    PRINTF("[0x%x] [Double] Previous track\n", id);
+    sendNOPKey(id);
     break;
   case BUTTON_A_C_BLACK:
-    keyboard.write(KEY_MEDIA_VOLUME_DOWN);
-    PRINTF("[0x%x] [Double] Volume Down\n", id);
+    sendNOPKey(id);
     break;
   case BUTTON_A_D_RED:
     sendNOPKey(id);
@@ -118,15 +157,14 @@ void doubleClickHandler(void *p) {
     sendNOPKey(id);
     break;
   case BUTTON_B_B_BLUE:
-    keyboard.write(KEY_ZOOM_IN);
-    PRINTF("[0x%x] [Double] Zoom in\n", id);
+    sendNOPKey(id);
     break;
   case BUTTON_B_C_BLACK:
-    sendFnKeyPress('P');
-    PRINTF("[0x%x] [Double] Play podcast\n", id);
+    sendNOPKey(id);
     break;
   case BUTTON_B_D_RED:
-    sendNOPKey(id);
+    keyboard.write(KEY_MEDIA_EJECT);
+    PRINTF("[0x%x] [Double] Eject\n", id);
     break;
   default:
     sendNOPKey(id);
@@ -138,8 +176,13 @@ void setupButtons() {
     auto point = reinterpret_cast<void *>(static_cast<uintptr_t>(id));
 
     btn->attachClick(clickHandler, point);
-    btn->attachDoubleClick(doubleClickHandler, point);
+
+    // Enable double click for some of the buttons
+    if (id == BUTTON_A_A_BLACK || id == BUTTON_B_D_RED) {
+      btn->attachDoubleClick(doubleClickHandler, point);
+      btn->setClickTicks(CLICK_TICKS);
+    }
+
     btn->setDebounceTicks(DEBOUNCE_TICKS);
-    btn->setClickTicks(CLICK_TICKS);
   }
 }
