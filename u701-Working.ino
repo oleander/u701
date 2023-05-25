476674129c4a999f67c36f1738a7a6bdab495ae3
Linus Oleander <linus.oleander@clark.de>
2023-05-25 11:04:55 +0200

    Simplify BLE pipeline

--- u701-Working.ino
+++ u701-Working.ino
@@ -34,52 +34,6 @@ static uint8_t ON[] = {0x1, 0x0};
 
 static BLEAdvertisedDevice *device = nullptr;
 
-BLEClient *client = BLEDevice::createClient();
-
-const char *getErrorMessage(uint8_t error_code) {
-  auto it = hci_error_codes.find(error_code);
-  if (it != hci_error_codes.end()) {
-    return it->second;
-  }
-  return nullptr;
-}
-
-bool connectToServer() {
-  if (device == nullptr) {
-    PRINTLN("[BUG] Address is null");
-    return false;
-  }
-
-  PRINTLN("Discovering services ...");
-  auto service = client->getService(hidService);
-  if (!service) {
-    PRINTLN("[BUG] Service not found");
-    return false;
-  }
-
-  auto characteristic = service->getCharacteristic(reportUUID);
-  if (!characteristic->canNotify()) {
-    PRINTLN("[BUG] Characteristic cannot notify");
-    return false;
-  }
-
-  PRINTLN("Subscribing to notifications");
-  characteristic->registerForNotify(onNotification);
-
-  delay(50);
-
-  auto descriptor = characteristic->getDescriptor(cccdUUID);
-  if (!descriptor) {
-    PRINTLN("[BUG] Descriptor not found");
-    return false;
-  }
-
-  PRINTLN("Enabling notifications");
-  descriptor->writeValue(ON, sizeof(ON), true);
-
-  return true;
-}
-
 int32_t dataToInt(uint8_t *pData, size_t length) {
   int32_t result = 0;
 
@@ -113,13 +67,39 @@ static void onNotification(BLERemoteCharacteristic *characteristic, uint8_t *dat
 
 class ClientCallback : public BLEClientCallbacks {
   void onConnect(BLEClient *client) {
-    PRINTLN("Connected to BLE buttons");
-    state = CONNECTED;
+    PRINTLN("Discovering services ...");
+    auto service = client->getService(hidService);
+    if (!service) {
+      PRINTLN("[BUG] Service not found");
+      return false;
+    }
+
+    PRINTLN("Discovering characteristics ...");
+    auto characteristic = service->getCharacteristic(reportUUID);
+    if (!characteristic->canNotify()) {
+      PRINTLN("[BUG] Characteristic cannot notify");
+      return false;
+    }
+
+    PRINTLN("Subscribing to notifications");
+    characteristic->registerForNotify(onNotification);
+
+    delay(50);
+
+    auto descriptor = characteristic->getDescriptor(cccdUUID);
+    if (!descriptor) {
+      PRINTLN("[BUG] Descriptor not found");
+      return false;
+    }
+
+    PRINTLN("Enabling notifications ...");
+    descriptor->writeValue(ON, sizeof(ON), true);
   }
 
   void onDisconnect(BLEClient *client) {
-    PRINTLN("Disconnected from BLE buttons");
-    state = DISCONNECTED;
+    PRINTLN("Disconnected, restarting ESP32 ...");
+    delay(1000);
+    ESP.restart();
   }
 };
 
@@ -141,113 +121,50 @@ class AdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
            manu.size(), serv.size());
 
     PRINTLN("Found controller unit");
-    device = new BLEAdvertisedDevice(advertised);
+    auto device = new BLEAdvertisedDevice(advertised);
+    auto client = BLEDevice::createClient();
+    client->setClientCallbacks(new ClientCallback());
+    client->connect(device);
 
     PRINTLN("Stopping scan");
     advertised.getScan()->stop();
   }
 };
 
-// Callback function for GATT client
-void gattcEventHandler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
-                       esp_ble_gattc_cb_param_t *param) {
-  const char *error_msg = getErrorMessage(event);
-  if (error_msg) {
-    PRINTF("EVT: %s\n", error_msg);
-  } else {
-    PRINTF("EVT %x\n", event);
-  }
-}
-
-void my_gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param) {
-  const char *error_msg = getErrorMessage(event);
-  if (error_msg) {
-    PRINTF("GAP EVT: %s, PARAMS: %x\n", error_msg, param->scan_rst.search_evt);
-  } else {
-    PRINTF("GAP EVT %x, PARAMS: %x\n", event, param->scan_rst.search_evt);
-  }
-}
-
-void my_gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if,
-                            esp_ble_gatts_cb_param_t *param) {
-  const char *error_msg = getErrorMessage(event);
-  if (error_msg) {
-    PRINTF("GATTS EVT: %s\n", error_msg);
-  } else {
-    PRINTF("GATTS EVT %x\n", event);
-  }
-}
-
-void setup() {
-#if defined(INFO)
-  Serial.begin(SERIAL_BAUD_RATE);
-#endif
-
-  PRINTLN("\nStarting ESP32 ...");
-
-  PRINTLN("Enable Keyboard");
-  keyboard.begin();
-  keyboard.setDelay(12);
-
-  PRINTLN("Starting...");
-
+void scanForDevice() {
   auto scan = BLEDevice::getScan();
 
   scan->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks());
   scan->setInterval(SCAN_INTERVAL);
   scan->setWindow(SCAN_WINDOW);
   scan->setActiveScan(true);
-
-  client->setClientCallbacks(new ClientCallback());
-
-  setupButtons();
-
-  PRINTLN("");
-
-#if defined(DEBUG)
-  BLEDevice::setCustomGattcHandler(gattcEventHandler);
-  BLEDevice::setCustomGattsHandler(my_gatts_event_handler);
-  BLEDevice::setCustomGapHandler(my_gap_event_handler);
-#endif
-
   scan->start(0, false);
   scan->clearResults();
+}
 
-  if (device == nullptr) {
-    PRINTLN("[BUG] Device is null, will restart");
-    delay(1000); // Give the serial monitor some time
-    ESP.restart();
-  }
+void setupKeyboard() {
+  PRINTLN("Enable Keyboard");
+  keyboard.begin();
+  keyboard.setDelay(12);
+}
 
-  PRINTLN("Connecting to Terrain Command buttons");
-  client->connect(device);
+void setupSerial() {
+#if defined(INFO)
+  Serial.begin(SERIAL_BAUD_RATE);
+#endif
+}
+
+void setup() {
+  PRINTLN("\nStarting ESP32 ...");
+
+  setupSerial();
+  setupKeyboard();
+  setupButtons();
+  scanForDevice();
 }
 
 void loop() {
-  switch (state) {
-  case SUBSCRIBED:
-    if (activeButton) {
-      activeButton->tick(activeState);
-    }
-    break;
-  case CONNECTED:
-    if (!connectToServer()) {
-      PRINTLN("Failed to connect to server, will restart");
-      delay(1000); // Give the serial monitor some time
-      ESP.restart();
-    }
-
-    state = SUBSCRIBED;
-    PRINTLN("Subscribed to notifications");
-    break;
-  case DISCONNECTED:
-    PRINTLN("Device disconnected, will restart");
-    delay(1000); // Give the serial monitor some time
-    ESP.restart();
-    break;
-  case CONNECTING:
-    PRINT(".");
-    delay(100);
-    break;
+  if (activeButton) {
+    activeButton->tick(activeState);
   }
 };
