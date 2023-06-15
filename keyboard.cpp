96fd922e361403fb8da13571c0cd51b7de899b7e
Linus Oleander <linus.oleander@clark.de>
2023-06-15 11:14:13 +0200

    Clean up switch events

--- keyboard.cpp
+++ keyboard.cpp
@@ -1,18 +1,18 @@
 #include "keyboard.h"
 
-std::unordered_map<ID, OneButton *> buttons = {{BUTTON_A_D_BLACK, new OneButton(PIN, false, false)},
-                                               {BUTTON_A_C_BLUE, new OneButton(PIN, false, false)},
-                                               {BUTTON_A_B_BLACK, new OneButton(PIN, false, false)},
-                                               {BUTTON_A_A_RED, new OneButton(PIN, false, false)},
-                                               {BUTTON_B_D_BLACK, new OneButton(PIN, false, false)},
-                                               {BUTTON_B_C_BLUE, new OneButton(PIN, false, false)},
-                                               {BUTTON_B_B_BLACK, new OneButton(PIN, false, false)},
-                                               {BUTTON_B_A_RED, new OneButton(PIN, false, false)}};
+std::unordered_map<ID, OneButton *> buttons = {
+    {BUTTON_A_A_RED, new OneButton(PIN, false, false)},
+    {BUTTON_A_B_BLACK, new OneButton(PIN, false, false)},
+    {BUTTON_A_C_BLUE, new OneButton(PIN, false, false)},
+    {BUTTON_A_D_BLACK, new OneButton(PIN, false, false)},
+    {BUTTON_B_A_RED, new OneButton(PIN, false, false)},
+    {BUTTON_B_B_BLACK, new OneButton(PIN, false, false)},
+    {BUTTON_B_C_BLUE, new OneButton(PIN, false, false)},
+    {BUTTON_B_D_BLACK, new OneButton(PIN, false, false)},
+};
 
 typedef OneButton *Button;
 typedef u_int16_t ID;
-
-bool useKeyboardForLogging = false;
 
 enum FunctionState { NoFn, CmdFn, MediaFn };
 
@@ -25,13 +25,9 @@ void sendNOPKey(ID id) {
   keyboard.write(KEY_INVALID);
 }
 
-void sendCmdFnKeyPress(char letter) { keyboard.print(letter); }
-
-void sendMediaFnKeyPress(char letter, ID id) {
-  if (functionState != MediaFn) {
-    PRINTF("0x%x] [BUG] MediaFn is not enabled using letter %s\n", id, letter);
-    return;
-  }
+void sendKeyPress(char letter, ID id) {
+  PRINTF("[%sFn] Sending letter %s", functionState == CmdFn ? "Cmd" : "Media",
+         String(letter).c_str());
 
   keyboard.print(letter);
   functionState = NoFn;
@@ -41,25 +37,14 @@ void clickHandler(void *p) {
   ID id = POINTER(p);
 
   switch (id) {
-  case BUTTON_A_D_BLACK:
-    keyboard.write(KEY_MEDIA_PLAY_PAUSE);
-    PRINTF("[0x%x] [Click] Play/Pause\n", id);
-    break;
-  case BUTTON_A_C_BLUE:
-    if (functionState == MediaFn) {
-      sendMediaFnKeyPress('2', id);
-    } else if (functionState == CmdFn) {
-      keyboard.write(KEY_ZOOM_OUT);
-      PRINTF("[0x%x] [Click] [CmdFn] Zoom Out\n", id);
-    } else {
-      keyboard.write(KEY_MEDIA_PREVIOUS_TRACK);
-      PRINTF("[0x%x] [Click] Previous Track\n", id);
-    }
+  case BUTTON_A_A_RED:
+    functionState = MediaFn;
+    PRINTF("[0x%x] [Click] Media Fn\n", id);
 
     break;
   case BUTTON_A_B_BLACK:
     if (functionState == MediaFn) {
-      sendMediaFnKeyPress('1', id);
+      sendKeyPress('2', id);
     } else if (functionState == CmdFn) {
       keyboard.write(KEY_ZOOM_IN);
       PRINTF("[0x%x] [Click] [CmdFn] Zoom In\n", id);
@@ -69,56 +54,60 @@ void clickHandler(void *p) {
     }
 
     break;
-  case BUTTON_A_A_RED:
-    if (functionState == CmdFn) {
-      keyboard.print('C');
-      PRINTF("[0x%x] [Click] [CmdFn] Restore map\n", id);
-    } else {
-      functionState = MediaFn;
-      PRINTF("[0x%x] [Click] Media Fn\n", id);
-    }
-    break;
-  case BUTTON_B_D_BLACK:
+  case BUTTON_A_C_BLUE:
     if (functionState == MediaFn) {
-      sendMediaFnKeyPress('5', id);
+      sendKeyPress('3', id);
     } else if (functionState == CmdFn) {
-      sendCmdFnKeyPress('E');
-      PRINTF("[0x%x] [Click] [CmdFn] Navigation");
-      PRINTF("[0x%x] [Click] [CmdFn] Navigation\n", id);
+      keyboard.write(KEY_ZOOM_OUT);
+      PRINTF("[0x%x] [Click] [CmdFn] Zoom Out\n", id);
     } else {
-      sendCmdFnKeyPress('N');
-      PRINTF("[0x%x] [Click] Toggle noise cancelling\n", id);
+      keyboard.write(KEY_MEDIA_PREVIOUS_TRACK);
+      PRINTF("[0x%x] [Click] Previous Track\n", id);
     }
+
     break;
-  case BUTTON_B_C_BLUE:
-    if (functionState == MediaFn) {
-      sendMediaFnKeyPress('4', id);
-    } else if (functionState == CmdFn) {
-      sendCmdFnKeyPress('B');
-      PRINTF("[0x%x] [Click] [CmdFn] Random Podcast\n", id);
-    } else {
-      keyboard.write(KEY_MEDIA_NEXT_TRACK);
-      PRINTF("[0x%x] [Click] Next Track\n", id);
-    }
-    break;
-  case BUTTON_B_B_BLACK:
-    if (functionState == MediaFn) {
-      sendMediaFnKeyPress('3', id);
-    } else if (functionState == CmdFn) {
-      sendCmdFnKeyPress('A');
-      PRINTF("[0x%x] [Click] [CmdFn] Random Music\n", id);
-    } else {
-      keyboard.write(KEY_MEDIA_VOLUME_UP);
-      PRINTF("[0x%x] [Click] Volume UP\n", id);
-    }
+  case BUTTON_A_D_BLACK:
+    keyboard.write(KEY_MEDIA_PLAY_PAUSE);
+    PRINTF("[0x%x] [Click] Play/Pause\n", id);
     break;
   case BUTTON_B_A_RED:
     functionState = (functionState == CmdFn) ? NoFn : CmdFn;
-
     if (functionState == CmdFn) {
       PRINTF("[0x%x] [Click] CmdFn ON\n", id);
     } else {
       PRINTF("[0x%x] [Click] CmdFn OFF\n", id);
+    }
+
+    break;
+  case BUTTON_B_B_BLACK:
+    if (functionState == MediaFn) {
+      sendKeyPress('5', id);
+    } else if (functionState == CmdFn) {
+      sendKeyPress('F', id);
+    } else {
+      keyboard.write(KEY_MEDIA_VOLUME_UP);
+      PRINTF("[0x%x] [Click] Volume UP\n", id);
+    }
+
+    break;
+  case BUTTON_B_C_BLUE:
+    if (functionState == MediaFn) {
+      sendKeyPress('7', id);
+    } else if (functionState == CmdFn) {
+      sendKeyPress('G', id);
+    } else {
+      keyboard.write(KEY_MEDIA_NEXT_TRACK);
+      PRINTF("[0x%x] [Click] Next Track\n", id);
+    }
+
+    break;
+  case BUTTON_B_D_BLACK:
+    if (functionState == MediaFn) {
+      sendKeyPress('8', id);
+    } else if (functionState == CmdFn) {
+      sendKeyPress('H', id);
+    } else {
+      keyboard.print('N');
     }
 
     break;
@@ -136,6 +125,7 @@ void doubleClickHandler(void *p) {
   case BUTTON_B_D_BLACK:
     keyboard.write(KEY_MEDIA_EJECT);
     PRINTF("[0x%x] [Double] Eject\n", id);
+
     break;
   default:
     sendNOPKey(id);
