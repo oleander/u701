6fcc63c987126dd249e6d28204cd457e63964b8b
Linus Oleander <linus.oleander@clark.de>
2023-10-11 16:46:06 +0200

    Comment out State struct and Action enum in state.h

--- src/state.h
+++ src/state.h
@@ -1,15 +1,1 @@
-enum class Action { TICK, RESTART, INIT_OTA, LOOP_OTA, WAIT_FOR_PHONE };
-
-// struct State {
-//   int pushedAt;
-//   ID id;
-//   bool active;
-//   Action action;
-
-//   int ranAgoInMillis() const;
-//   bool isActive() const;
-//   void setInactive();
-//   void setActive();
-//   void reset();
-//   void tick();
-// };
+// enum class Action { TICK, RESTART, INIT_OTA, LOOP_OTA, WAIT_FOR_PHONE };
