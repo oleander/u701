3467b4553ad83a18ee5f36a97b7ec18b96c29cfa
Linus Oleander <linus.oleander@clark.de>
2023-10-11 17:29:47 +0200

    Reformat settings.h for better readability

--- src/settings.h
+++ src/settings.h
@@ -1,16 +1,14 @@
-#pragma once
-
-#define DEVICE_MAC "f7:97:ac:1f:f8:c0"
+#define DEVICE_MAC          "f7:97:ac:1f:f8:c0"
 #define DEVICE_MANUFACTURER "Oleander"
-#define DEVICE_NAME "u701"
-#define DEVICE_BATTERY 100
+#define DEVICE_NAME         "u701"
+#define DEVICE_BATTERY      100
 
-#define DEBOUNCE_TICKS 50 // in ms
-#define CLICK_TICKS 500 // in ms
+#define DEBOUNCE_TICKS   50  // in ms
+#define CLICK_TICKS      500 // in ms
 #define LONG_PRESS_TICKS 800 // in ms
 
 #define SERIAL_BAUD_RATE 115200
-#define SCAN_INTERVAL 3000 // in ms
-#define SCAN_WINDOW 100 // in ms
-#define WIFI_PASSWORD "11111111"
-#define WIFI_SSID "u701"
+#define SCAN_INTERVAL    3000 // in ms
+#define SCAN_WINDOW      100  // in ms
+#define WIFI_PASSWORD    "11111111"
+#define WIFI_SSID        "u701"
