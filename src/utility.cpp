1a01d42872f9844f6ad3ac8bc7e2c5fc3eb59680
Linus Oleander <linus.oleander@clark.de>
2023-10-11 16:43:00 +0200

    Remove device disconnection from restart function

--- src/utility.cpp
+++ src/utility.cpp
@@ -2,17 +2,6 @@
 
 void restart(const char *reason) {
   Log.noticeln(reason);
-
-  if (client && client->isConnected()) {
-    Log.noticeln("Disconnecting from device ...");
-    if (client->disconnect()) {
-      Log.noticeln("Could not disconnect from device!");
-    } else {
-      Log.noticeln("Failed to disconnect from device!");
-    }
-  }
-
   Log.noticeln("Restarting ESP32 ...");
-
   ESP.restart();
 }
