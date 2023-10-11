3847a0502fc6d5cfa2945fac45c46ec99c16e850
Linus Oleander <linus.oleander@clark.de>
2023-10-11 22:39:30 +0200

    Refactor code and remove upload_port in platformio.ini

--- include/ClientCallback.h
+++ include/ClientCallback.h
@@ -1,11 +1,11 @@
+/* Removes warnings */
+#undef LOG_LEVEL_INFO
+#undef LOG_LEVEL_ERROR
+
 #include "BLEServer.h"
 #include "NimBLEClient.h"
 #include "utility.h"
 #include <ArduinoLog.h>
-
-/* Removes warnings */
-#undef LOG_LEVEL_INFO
-#undef LOG_LEVEL_ERROR
 
 class ClientCallback : public NimBLEClientCallbacks {
 public:
