4da5243b58ed8ce792b280b01986487fbf93b2ee
Linus Oleander <linus.oleander@clark.de>
2023-10-11 16:36:04 +0200

    Remove button tick and reset calls in State class

--- src/state.cpp
+++ src/state.cpp
@@ -4,22 +4,17 @@ bool State::isActive() const {
   return active;
 };
 
-void State::reset() {
-  button->reset();
-};
+void State::reset(){};
 
 void State::tick() {
-  button->tick(active);
 }
 
 void State::setActive() {
   pushedAt = millis();
-  button->tick(true);
-  active = true;
+  active   = true;
 }
 
 void State::setInactive() {
-  button->tick(false);
   active = false;
 }
 
