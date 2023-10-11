cfbc47ae08a879bc0092a4fb529addb9f4d53ac3
Linus Oleander <linus.oleander@clark.de>
2023-10-11 16:47:24 +0200

    Remove unused keyboard key definitions

--- src/keyboard.h
+++ src/keyboard.h
@@ -10,12 +10,6 @@
 #include <BleKeyboard.h>
 
 // This pin is never used, but it is required by the OneButton library
-#define KEY_INVALID  0xAA
-#define KEY_SIRI     0xCF
-#define KEY_ZOOM_OUT KEY_NUM_MINUS
-#define KEY_ZOOM_IN  KEY_NUM_PLUS
-#define POINTER(p)   (static_cast<uint16_t>(reinterpret_cast<uintptr_t>(p)))
-
 extern BleKeyboard keyboard;
 extern bool useKeyboardForLogging;
 
