ed677601ffa888495366a1fe8631d7cb8e0b94e3
Linus Oleander <linus.oleander@clark.de>
2023-10-27 02:28:21 +0200

    Remove unused linker and delete build.rs file

--- build.rs
+++
@@ -1,5 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-  // embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
-  // embuild::build::LinkArgs::output_propagated("ESP_IDF")?;
-  Ok(())
-}
