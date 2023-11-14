ffd9cf2669cea8f291d54ce84ef3922cfa3d41a5
Linus Oleander <linus.oleander@clark.de>
2023-11-15 00:26:57 +0100

    More

--- include/ffi.h
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-
-extern "C" void ble_keyboard_write(uint8_t c[2]);
-extern "C" void ble_keyboard_print(const uint8_t *format);
-extern "C" bool ble_keyboard_is_connected();
-extern "C" void handle_external_click_event(const uint8_t *event, size_t length);
-extern "C" void setup_rust();
--- src/utility.cpp
+++
@@ -1,7 +0,0 @@
-#include "utility.h"
-
-void restart(const char *reason) {
-  Log.noticeln(reason);
-  Log.noticeln("Restarting ESP32 ...");
-  ESP.restart();
-}
