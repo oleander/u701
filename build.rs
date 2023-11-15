e2c9d1dbcff4c44a2558684623a020849e73dcd3
Linus Oleander <linus.oleander@clark.de>
2023-11-15 06:22:26 +0100

    Update build configs, add bindgen and improve debug logging

--- build.rs
+++ build.rs
@@ -1,23 +1,49 @@
 use glob::glob;
 use std::path::PathBuf;
 use std::env;
+use std::io::{self, Write};
 
-// Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
+macro_rules! debug {
+  ($($arg:tt)*) => {
+    if cfg!(debug_assertions) {
+      writeln!(io::stderr(), $($arg)*).unwrap();
+    }
+  }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
   embuild::build::LinkArgs::output_propagated("ESP_IDF")?;
 
   let env = "release";
 
-  let pattern = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join(".pio").join("libdeps").join(env).join("**").join("include").join("*.h");
+  println!("cargo:rustc-link-lib=bzip2");
 
-  for entry in glob(pattern.to_str().unwrap()).expect("Failed to read glob pattern") {
+  let headers = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
+    .join(".pio")
+    .join("libdeps")
+    .join(env)
+    .join("*")
+    .join("include")
+    .join("*.h");
+
+  debug!("Headers: {:?}", headers);
+
+  // Use the glob library to find all header files that match the pattern.
+  for entry in glob(headers.to_str().unwrap()).expect("Failed to read glob pattern") {
     match entry {
       Ok(path) => {
-        println!("Found header: {:?}", path.display());
-        // You can now call bindgen for each header file or collect them
+        debug!("\tGenerating bindings for: {:?}", path);
+        let bindings =
+          bindgen::Builder::default().header(path.to_str().unwrap()).generate().expect("Unable to generate bindings");
+
+        let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+        bindings
+          .write_to_file(out_path.join(format!("{}_bindings.rs", path.file_stem().unwrap().to_str().unwrap())))
+          .expect("Couldn't write bindings!");
+        debug!("\tBindings generated");
       },
-      Err(e) => println!("Error: {:?}", e)
+      Err(e) => debug!("{:?}", e)
     }
   }
 
