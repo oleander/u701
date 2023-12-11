cc567a776d7f62998de4ae62885d34312415863d
Your Name <you@example.com>
2023-12-11 16:45:34 +0100

    Optimize constants and adjust ffi shortcut func

--- machine/src/constants.rs
+++ machine/src/constants.rs
@@ -37,20 +37,20 @@ lazy_static! {
 
   pub static ref META: HashMap<u8, HashMap<u8, u8>> = {
     let mut meta1 = HashMap::new();
-    meta1.insert(A2, 48); // '1' + 48 = 'a'
-    meta1.insert(A3, 49); // '1' + 49 = 'b'
-    meta1.insert(A4, 50); // '1' + 50 = 'c'
-    meta1.insert(B2, 51); // '1' + 51 = 'd'
-    meta1.insert(B3, 52); // '1' + 52 = 'e'
-    meta1.insert(B4, 53); // '1' + 53 = 'f'
+    meta1.insert(A2, 0); // '1' + 48 = 'a'
+    meta1.insert(A3, 1); // '1' + 49 = 'b'
+    meta1.insert(A4, 2); // '1' + 50 = 'c'
+    meta1.insert(B2, 3); // '1' + 51 = 'd'
+    meta1.insert(B3, 4); // '1' + 52 = 'e'
+    meta1.insert(B4, 5); // '1' + 53 = 'f'
 
     let mut meta2 = HashMap::new();
-    meta2.insert(A2, 0); // '1' + 0 = '1'
-    meta2.insert(A3, 1); // '1' + 1 = '2'
-    meta2.insert(A4, 2); // '1' + 2 = '3'
-    meta2.insert(B2, 3); // '1' + 3 = '4'
-    meta2.insert(B3, 4); // '1' + 4 = '5'
-    meta2.insert(B4, 5); // '1' + 5 = '6'
+    meta2.insert(A2, 6); // '1' + 0 = '1'
+    meta2.insert(A3, 7); // '1' + 1 = '2'
+    meta2.insert(A4, 8); // '1' + 2 = '3'
+    meta2.insert(B2, 9); // '1' + 3 = '4'
+    meta2.insert(B3, 10); // '1' + 4 = '5'
+    meta2.insert(B4, 11); // '1' + 5 = '6'
 
     let mut table = HashMap::new();
     table.insert(M1, meta1);
