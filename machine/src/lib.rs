5c8eef84a14aa77e6ba520a016185c15fec67352
Your Name <you@example.com>
2023-12-11 07:10:49 +0100

    Refactor State enum, remove redundant tests

--- machine/src/lib.rs
+++ machine/src/lib.rs
@@ -8,15 +8,11 @@ use constants::buttons::{M1, M2};
 use constants::{EVENT, META};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub enum Event {
-  Duo(u8, u8),
+pub enum State {
+  Combo(u8, u8),
   Key(u8),
   Meta(u8),
   Float
-}
-
-pub struct State {
-  curr: Event
 }
 
 #[derive(PartialEq, Debug)]
@@ -26,45 +22,39 @@ pub enum Data {
 }
 
 impl State {
-  pub fn new(event: Event) -> Self {
-    Self {
-      curr: event
-    }
-  }
-
   // Translates key idn into key press states
-  fn transition(&mut self, next: u8) -> Event {
-    self.curr = match (self.curr, next) {
+  fn transition(mut self, next: u8) -> State {
+    self = match (self, next) {
       // Any -> Meta: ignore previous
-      (_, meta_id @ (M1 | M2)) => Event::Meta(meta_id),
+      (_, meta_id @ (M1 | M2)) => State::Meta(meta_id),
 
       // Meta -> Regular: run shortcut
-      (Event::Meta(meta_id), event_id) => Event::Duo(meta_id, event_id),
+      (State::Meta(meta_id), event_id) => State::Combo(meta_id, event_id),
 
       // Regular -> Regular: run key
-      (_, media_id) => Event::Key(media_id)
+      (_, media_id) => State::Key(media_id)
     };
 
-    self.curr.clone()
+    self.clone()
   }
 
   // Converts key presses into payloads
-  pub fn event(&mut self, next: u8) -> Option<Data> {
+  pub fn event(&self, next: u8) -> Option<Data> {
     match self.transition(next) {
       // Meta -> Regular: run shortcut
-      Event::Duo(meta_id, event_id) => META.get(&meta_id).and_then(|meta| meta.get(&event_id).map(|id| Data::Short(*id))),
+      State::Combo(meta_id, event_id) => META.get(&meta_id).and_then(|meta| meta.get(&event_id).map(|id| Data::Short(*id))),
       // Regular: run key
-      Event::Key(event_id) => EVENT.get(&event_id).map(|&a| Data::Media(a)),
+      State::Key(event_id) => EVENT.get(&event_id).map(|&a| Data::Media(a)),
       // Meta: wait for next press
-      Event::Meta(_) => None,
-      Event::Float => None
+      State::Meta(_) => None,
+      State::Float => None
     }
   }
 }
 
 impl Default for State {
   fn default() -> Self {
-    Self::new(Event::Float)
+    State::Float
   }
 }
 
@@ -76,36 +66,36 @@ mod tests {
 
   #[test]
   fn test_new() {
-    let state = State::new(Event::Float);
-    assert_eq!(state.curr, Event::Float);
+    let state = State::default();
+    assert_eq!(state, State::Float);
   }
 
   #[test]
   fn test_default() {
     let state = State::default();
-    assert_eq!(state.curr, Event::Float);
+    assert_eq!(state, State::Float);
   }
 
   #[test]
   fn test_transition() {
-    let mut state = State::new(Event::Float);
+    let  state = State::default();
 
     // Test transition from any to Meta
     state.transition(M1);
-    assert_eq!(state.curr, Event::Meta(M1));
+    assert_eq!(state, State::Meta(M1));
 
     // Test transition from Meta to Regular
     state.transition(A2);
-    assert_eq!(state.curr, Event::Duo(M1, A2));
+    assert_eq!(state, State::Combo(M1, A2));
 
     // Test transition from Regular to Regular
     state.transition(B2);
-    assert_eq!(state.curr, Event::Key(B2));
+    assert_eq!(state, State::Key(B2));
   }
 
   #[test]
   fn test_event() {
-    let mut state = State::new(Event::Float);
+    let mut state = State::default();
 
     // Test Meta -> Regular
     assert_eq!(state.event(M1), None);
