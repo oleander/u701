cbbefc5abf073552e96bfc51b093640d5a5c35d6
Linus Oleander <linus@oleander.io>
2023-12-24 22:06:34 +0100

    Update ESP_IDF version and commented out unused dependencies. Added new modules in lib.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,85 @@
-mod ffi;
+#![no_main]
+#![feature(never_type)]
+
+extern crate esp_idf_hal;
+extern crate lazy_static;
+extern crate log;
+
 mod keyboard;
 
-use std::sync::mpsc::{channel, Receiver, Sender};
-use log::{debug, info};
-use lazy_static::lazy_static;
-use anyhow::{bail, Result};
-use std::sync::Mutex;
-use machine::Action;
-use ffi::*;
-use keyboard::*;
+use embassy_time::{Duration, Timer};
+use esp32_nimble::utilities::mutex::Mutex;
+use esp32_nimble::utilities::BleUuid;
+use esp32_nimble::utilities::BleUuid::Uuid16;
+use esp32_nimble::{BLEClient, BLEDevice};
+use esp_idf_hal::task::block_on;
+use keyboard::Keyboard;
+use log::{info, warn};
+use std::sync::Arc;
 
-lazy_static! {
-  static ref CHANNEL: (Sender<u8>, Mutex<Receiver<u8>>) = {
-    let (send, recv) = channel();
-    let recv = Mutex::new(recv);
-    (send, recv)
-  };
-}
+const SERVICE_UUID: BleUuid = Uuid16(0x1812);
+const CHAR_UUID: BleUuid = Uuid16(0x2A4D);
+const BLE_BUTTONS_NAME: &str = "key";
 
-fn main() -> Result<()> {
-  info!("[main] Starting main loop");
+#[no_mangle]
+fn app_main() {
+  esp_idf_sys::link_patches();
+  esp_idf_svc::log::EspLogger::initialize_default();
+  esp_idf_svc::timer::embassy_time::driver::link();
 
-  let receiver = CHANNEL.1.lock().unwrap();
-  let mut state = machine::State::default();
-  let mut keyboard = Keyboard::new();
+  info!("Setup connection to BLE buttons");
+  block_on(async {
+    let keyboard = Mutex::new(Keyboard::new());
 
-  info!("[main] Entering loop, waiting for events");
-  while let Ok(event_id) = receiver.recv() {
-    match state.transition(event_id) {
-      Some(Action::Media(keys)) => keyboard.write("media"),
-      Some(Action::Short(index)) => keyboard.write("short"),
-      None => debug!("[main] No action {}", event_id)
+    let device = BLEDevice::take();
+    let scan = device.get_scan();
+
+    let connect_device = Arc::new(Mutex::new(None));
+    let connect_device_clone = Arc::clone(&connect_device);
+
+    scan.active_scan(true).interval(490).window(450).on_result(move |found_device| {
+      if !found_device.is_advertising_service(&SERVICE_UUID) {
+        return;
+      }
+
+      if !found_device.name().starts_with(BLE_BUTTONS_NAME) {
+        return;
+      }
+
+      info!("Found device: {:?}", found_device.name());
+      *connect_device_clone.lock() = Some(found_device.clone());
+      BLEDevice::take().get_scan().stop().expect("Failed to stop scan");
+    });
+
+    scan.start(i32::MAX).await.expect("Failed to start scan");
+
+    let Some(device) = &*connect_device.lock() else {
+      return warn!("No device found");
+    };
+
+    let mut client = BLEClient::new();
+
+    client.connect(device.addr()).await.expect("Failed to connect to device");
+
+    let service = client.get_service(SERVICE_UUID).await.expect("Failed to get service");
+    let characteristic = service.get_characteristic(CHAR_UUID).await.expect("Failed to get characteristic");
+
+    characteristic
+      .on_notify(move |_data| {
+        info!("Received notification from device");
+        let mut keyboard = keyboard.lock();
+        if keyboard.connected() {
+          keyboard.write("Hello world");
+        }
+      })
+      .subscribe_notify(false)
+      .await
+      .expect("Failed to subscribe to notifications");
+
+    info!("Waiting for notifications ...");
+
+    loop {
+      Timer::after(Duration::from_secs(1)).await;
     }
-  }
-
-  bail!("[main] Event loop ended");
-}
-
-pub fn on_event(event: Option<&[u8; 4]>) {
-  match event {
-    Some(&[_, _, 0, _]) => debug!("Button was released"),
-    Some(&[_, _, n, _]) => CHANNEL.0.send(n).unwrap(),
-    // None => error!("[on_event] [BUG] Received {:?} event", event)
-    None => CHANNEL.0.send(0x50).unwrap()
-  }
+  });
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,220 +1,220 @@
 
-#include "ffi.h"
-#include "settings.h"
+// #include "ffi.h"
+// #include "settings.h"
 
-// #include <ArduinoLog.h>
-// #include <BleKeyboard.h>
-#include <NimBLEDevice.h>
-#include <NimBLEScan.h>
-#include <NimBLEUtils.h>
-#include <esp_task_wdt.h>
+// // #include <ArduinoLog.h>
+// // #include <BleKeyboard.h>
+// #include <NimBLEDevice.h>
+// #include <NimBLEScan.h>
+// #include <NimBLEUtils.h>
+// #include <esp_task_wdt.h>
 
-#define WDT_TIMEOUT 30
-// #include <ArduinoOTA.h>
-// #include <WiFi.h>
+// #define WDT_TIMEOUT 30
+// // #include <ArduinoOTA.h>
+// // #include <WiFi.h>
 
-IPAddress ip(192, 168, 4, 1);
-IPAddress gateway(192, 168, 4, 1);
-IPAddress subnet(255, 255, 255, 0);
+// IPAddress ip(192, 168, 4, 1);
+// IPAddress gateway(192, 168, 4, 1);
+// IPAddress subnet(255, 255, 255, 0);
 
-static NimBLEUUID reportUUID("2A4D");
-static NimBLEUUID cccdUUID("2902");
-static NimBLEUUID hidService("1812");
-static NimBLEUUID batteryServiceUUID("180F");
-static NimBLEUUID batteryLevelCharUUID("2A19");
+// static NimBLEUUID reportUUID("2A4D");
+// static NimBLEUUID cccdUUID("2902");
+// static NimBLEUUID hidService("1812");
+// static NimBLEUUID batteryServiceUUID("180F");
+// static NimBLEUUID batteryLevelCharUUID("2A19");
 
-static NimBLEClient *client;
+// static NimBLEClient *client;
 
-/* Removes warnings */
-#undef LOG_LEVEL_INFO
-#undef LOG_LEVEL_ERROR
+// /* Removes warnings */
+// #undef LOG_LEVEL_INFO
+// #undef LOG_LEVEL_ERROR
 
-const auto buttonMacAddress = NimBLEAddress(DEVICE_MAC, 1);
+// const auto buttonMacAddress = NimBLEAddress(DEVICE_MAC, 1);
 
-// BleKeyboard keyboard(DEVICE_NAME, DEVICE_MANUFACTURER, DEVICE_BATTERY);
+// // BleKeyboard keyboard(DEVICE_NAME, DEVICE_MANUFACTURER, DEVICE_BATTERY);
 
-void restart(const char *reason) {
-  // Log.noticeln(reason);
-  // Log.noticeln("Restarting ESP32 ...");
-  ESP.restart();
-}
-
-class ClientCallback : public NimBLEClientCallbacks {
-  void onConnect(NimBLEClient *client) override {
-    esp_task_wdt_reset();
-    // Log.noticeln("Connected to device!");
-  }
-
-  void onDisconnect(NimBLEClient *client) override {
-    restart("Disconnected from device");
-  }
-};
-
-extern "C" void ble_keyboard_write(uint8_t c[2]) {
-  // if (keyboard.isConnected()) {
-  // keyboard.write(c);
-  // }
-}
-
-extern "C" void ble_keyboard_print(const uint8_t *format) {
-  // if (keyboard.isConnected()) {
-  //   keyboard.print(reinterpret_cast<const char *>(format));
-  // }
-}
-
-extern "C" bool ble_keyboard_is_connected() {
-  // return keyboard.isConnected();
-}
-
-/* Add function isActive to the State struct */
-static void handleButtonClick(BLERemoteCharacteristic *_, uint8_t *data, size_t length, bool isNotify) {
-  // if (length != 4) {
-  //   .traceln("Received length should be 4, got %d (will continue anyway)", length);
-  // }
-
-  if (!isNotify) {
-    // Log.traceln("Received invalid isNotify: %d (expected true)", isNotify);
-    return;
-  }
-
-  // Log.traceln("[Click] Received length: %d", length);
-  // Log.traceln("[Click] Received isNotify: %d", isNotify);
-
-  c_on_event(data, length);
-}
-
-// void initializeKeyboard() {
-//   esp_task_wdt_reset();
-//   // Log.noticeln("Enable Keyboard");
-
-//   keyboard.setBatteryLevel(100);
-//   keyboard.setDelay(12);
-//   keyboard.begin();
-
-//   while (!keyboard.isConnected()) {
-//     esp_task_wdt_reset();
-//     delay(10);
-//   }
+// void restart(const char *reason) {
+//   // Log.noticeln(reason);
+//   // Log.noticeln("Restarting ESP32 ...");
+//   ESP.restart();
 // }
 
-void initializeSerialCommunication() {
-  esp_task_wdt_reset();
-  Serial.begin(SERIAL_BAUD_RATE);
-  // Log.begin(LOG_LEVEL_VERBOSE, &Serial);
-  // Log.setLevel(LOG_LEVEL_VERBOSE);
-  // Log.noticeln("Starting ESP32 ...");
-  esp_task_wdt_reset();
-}
+// class ClientCallback : public NimBLEClientCallbacks {
+//   void onConnect(NimBLEClient *client) override {
+//     esp_task_wdt_reset();
+//     // Log.noticeln("Connected to device!");
+//   }
 
-/**
- * Sets up the client to connect to the BLE device with the specified MAC address.
- * If the connection fails or no services/characteristics are found, the device will restart.
- */
-void connectToClientDevice() {
-  esp_task_wdt_reset();
-  // Log.noticeln("[Connecting] to Terrain Command ...");
+//   void onDisconnect(NimBLEClient *client) override {
+//     restart("Disconnected from device");
+//   }
+// };
 
-  if (client == nullptr) {
-    restart("Device not found, will reboot");
-  }
+// extern "C" void ble_keyboard_write(uint8_t c[2]) {
+//   // if (keyboard.isConnected()) {
+//   // keyboard.write(c);
+//   // }
+// }
 
-  static ClientCallback clientCallbackInstance;
-  client->setClientCallbacks(&clientCallbackInstance);
-  if (client->isConnected()) {
-    // return Log.noticeln("Already connected to device");
-    return;
-  }
+// extern "C" void ble_keyboard_print(const uint8_t *format) {
+//   // if (keyboard.isConnected()) {
+//   //   keyboard.print(reinterpret_cast<const char *>(format));
+//   // }
+// }
 
-  if (!client->connect()) {
-    restart("Could not connect to the Terrain Command");
-  }
+// extern "C" bool ble_keyboard_is_connected() {
+//   // return keyboard.isConnected();
+// }
 
-  // Log.noticeln("Discovering services ...");
-  for (auto &service: *client->getServices(true)) {
-    // Log.noticeln("Discovering characteristics ...");
-    for (auto &characteristic: *service->getCharacteristics(true)) {
-      auto currentServiceUUID = service->getUUID().toString().c_str();
-      auto currentCharUUID    = characteristic->getUUID().toString().c_str();
+// /* Add function isActive to the State struct */
+// static void handleButtonClick(BLERemoteCharacteristic *_, uint8_t *data, size_t length, bool isNotify) {
+//   // if (length != 4) {
+//   //   .traceln("Received length should be 4, got %d (will continue anyway)", length);
+//   // }
 
-      esp_task_wdt_reset();
+//   if (!isNotify) {
+//     // Log.traceln("Received invalid isNotify: %d (expected true)", isNotify);
+//     return;
+//   }
 
-      // Register for click events
-      if (!service->getUUID().equals(hidService)) {
-        // Log.warningln("[Click] Unknown report service: %X", currentServiceUUID);
-      } else if (!characteristic->getUUID().equals(reportUUID)) {
-        // Log.warningln("[Click] Unknown report characteristic: %X", currentCharUUID);
-      } else if (!characteristic->canNotify()) {
-        // Log.warningln("[Click] Cannot subscribe to notifications: %X", currentCharUUID);
-      } else if (!characteristic->subscribe(true, handleButtonClick, false)) {
-        // Log.errorln("[Click] [Bug] Failed to subscribe to notifications: %X", currentCharUUID);
-      } else {
-        // return Log.noticeln("[Click] Subscribed to notifications: %X", currentCharUUID);
-        return;
-      }
-    }
-  }
+//   // Log.traceln("[Click] Received length: %d", length);
+//   // Log.traceln("[Click] Received isNotify: %d", isNotify);
 
-  // Log.noticeln("Subcribed to all characteristics");
-  esp_task_wdt_reset();
-}
+//   c_on_event(data, length);
+// }
 
-class Callbacks : public NimBLEAdvertisedDeviceCallbacks {
-  void onResult(NimBLEAdvertisedDevice *advertised) {
-    auto macAddr = advertised->getAddress();
+// // void initializeKeyboard() {
+// //   esp_task_wdt_reset();
+// //   // Log.noticeln("Enable Keyboard");
 
-    if (macAddr != buttonMacAddress) {
-      Serial.print(".");
-      return;
-    }
+// //   keyboard.setBatteryLevel(100);
+// //   keyboard.setDelay(12);
+// //   keyboard.begin();
 
-    client = NimBLEDevice::createClient(macAddr);
-    advertised->getScan()->stop();
+// //   while (!keyboard.isConnected()) {
+// //     esp_task_wdt_reset();
+// //     delay(10);
+// //   }
+// // }
 
-    // Log.noticeln("[SCAN] Terrain Command found");
-  }
-};
+// void initializeSerialCommunication() {
+//   esp_task_wdt_reset();
+//   Serial.begin(SERIAL_BAUD_RATE);
+//   // Log.begin(LOG_LEVEL_VERBOSE, &Serial);
+//   // Log.setLevel(LOG_LEVEL_VERBOSE);
+//   // Log.noticeln("Starting ESP32 ...");
+//   esp_task_wdt_reset();
+// }
 
-/**
- * Sets up the BLE scan to search for the device with the specified MAC address.
- * If the device is found, the scan will stop and the client will be set up.
- * The scan interval is set high to save power
- */
-void startBLEScanForDevice() {
-  // Log.noticeln("Starting BLE scan ...");
+// /**
+//  * Sets up the client to connect to the BLE device with the specified MAC address.
+//  * If the connection fails or no services/characteristics are found, the device will restart.
+//  */
+// void connectToClientDevice() {
+//   esp_task_wdt_reset();
+//   // Log.noticeln("[Connecting] to Terrain Command ...");
 
-  auto scan = NimBLEDevice::getScan();
-  static Callbacks scanCallbackInstance;
-  scan->setAdvertisedDeviceCallbacks(&scanCallbackInstance);
-  scan->setInterval(SCAN_INTERVAL);
-  scan->setWindow(SCAN_WINDOW);
-  scan->setActiveScan(true);
-  scan->start(0, false);
-}
+//   if (client == nullptr) {
+//     restart("Device not found, will reboot");
+//   }
 
-void setupWatchdog() {
-  // Log.noticeln("Setting up watchdog ...");
-  esp_task_wdt_init(WDT_TIMEOUT, true);
-  esp_task_wdt_add(nullptr);
-}
+//   static ClientCallback clientCallbackInstance;
+//   client->setClientCallbacks(&clientCallbackInstance);
+//   if (client->isConnected()) {
+//     // return Log.noticeln("Already connected to device");
+//     return;
+//   }
 
-void disableWatchdog() {
-  // Log.noticeln("Disabling watchdog ...");
-  esp_task_wdt_delete(nullptr);
-}
+//   if (!client->connect()) {
+//     restart("Could not connect to the Terrain Command");
+//   }
 
-void setupBle() {
-  esp_task_wdt_reset();
-  // NimBLEDevice::init(DEVICE_NAME);
-}
+//   // Log.noticeln("Discovering services ...");
+//   for (auto &service: *client->getServices(true)) {
+//     // Log.noticeln("Discovering characteristics ...");
+//     for (auto &characteristic: *service->getCharacteristics(true)) {
+//       auto currentServiceUUID = service->getUUID().toString().c_str();
+//       auto currentCharUUID    = characteristic->getUUID().toString().c_str();
 
-extern "C" void init_arduino() {
-  setupWatchdog();
-  setupBle();
-  // initializeKeyboard();
-  initializeSerialCommunication();
-  disableWatchdog();
-  startBLEScanForDevice();
-  setupWatchdog();
-  connectToClientDevice();
-  disableWatchdog();
-}
+//       esp_task_wdt_reset();
+
+//       // Register for click events
+//       if (!service->getUUID().equals(hidService)) {
+//         // Log.warningln("[Click] Unknown report service: %X", currentServiceUUID);
+//       } else if (!characteristic->getUUID().equals(reportUUID)) {
+//         // Log.warningln("[Click] Unknown report characteristic: %X", currentCharUUID);
+//       } else if (!characteristic->canNotify()) {
+//         // Log.warningln("[Click] Cannot subscribe to notifications: %X", currentCharUUID);
+//       } else if (!characteristic->subscribe(true, handleButtonClick, false)) {
+//         // Log.errorln("[Click] [Bug] Failed to subscribe to notifications: %X", currentCharUUID);
+//       } else {
+//         // return Log.noticeln("[Click] Subscribed to notifications: %X", currentCharUUID);
+//         return;
+//       }
+//     }
+//   }
+
+//   // Log.noticeln("Subcribed to all characteristics");
+//   esp_task_wdt_reset();
+// }
+
+// class Callbacks : public NimBLEAdvertisedDeviceCallbacks {
+//   void onResult(NimBLEAdvertisedDevice *advertised) {
+//     auto macAddr = advertised->getAddress();
+
+//     if (macAddr != buttonMacAddress) {
+//       Serial.print(".");
+//       return;
+//     }
+
+//     client = NimBLEDevice::createClient(macAddr);
+//     advertised->getScan()->stop();
+
+//     // Log.noticeln("[SCAN] Terrain Command found");
+//   }
+// };
+
+// /**
+//  * Sets up the BLE scan to search for the device with the specified MAC address.
+//  * If the device is found, the scan will stop and the client will be set up.
+//  * The scan interval is set high to save power
+//  */
+// void startBLEScanForDevice() {
+//   // Log.noticeln("Starting BLE scan ...");
+
+//   auto scan = NimBLEDevice::getScan();
+//   static Callbacks scanCallbackInstance;
+//   scan->setAdvertisedDeviceCallbacks(&scanCallbackInstance);
+//   scan->setInterval(SCAN_INTERVAL);
+//   scan->setWindow(SCAN_WINDOW);
+//   scan->setActiveScan(true);
+//   scan->start(0, false);
+// }
+
+// void setupWatchdog() {
+//   // Log.noticeln("Setting up watchdog ...");
+//   esp_task_wdt_init(WDT_TIMEOUT, true);
+//   esp_task_wdt_add(nullptr);
+// }
+
+// void disableWatchdog() {
+//   // Log.noticeln("Disabling watchdog ...");
+//   esp_task_wdt_delete(nullptr);
+// }
+
+// void setupBle() {
+//   esp_task_wdt_reset();
+//   // NimBLEDevice::init(DEVICE_NAME);
+// }
+
+// extern "C" void init_arduino() {
+//   setupWatchdog();
+//   setupBle();
+//   // initializeKeyboard();
+//   initializeSerialCommunication();
+//   disableWatchdog();
+//   startBLEScanForDevice();
+//   setupWatchdog();
+//   connectToClientDevice();
+//   disableWatchdog();
+// }
