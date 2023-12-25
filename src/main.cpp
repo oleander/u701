202f095727f0bcd96a3b098235632922927bd9b0
Linus Oleander <linus@oleander.io>
2023-12-25 14:43:43 +0100

    Change target architecture from ESP32 to ESP32C3 and update related settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,189 @@
-
+// #include <BleKeyboard.h>
 #include "ffi.h"
-#include "settings.h"
-
-#include <ArduinoLog.h>
 #include <BleKeyboard.h>
 #include <NimBLEDevice.h>
 #include <NimBLEScan.h>
 #include <NimBLEUtils.h>
-#include <esp_task_wdt.h>
+#include <vector>
 
-#define WDT_TIMEOUT 30
-
-IPAddress ip(192, 168, 4, 1);
-IPAddress gateway(192, 168, 4, 1);
-IPAddress subnet(255, 255, 255, 0);
-
-static NimBLEUUID reportUUID("2A4D");
-static NimBLEUUID cccdUUID("2902");
-static NimBLEUUID hidService("1812");
-static NimBLEUUID batteryServiceUUID("180F");
-static NimBLEUUID batteryLevelCharUUID("2A19");
-static BLEClientCallbacks clientCallbackInstance;
-
-static NimBLEClient *client;
-
-/* Removes warnings */
-#undef LOG_LEVEL_INFO
-#undef LOG_LEVEL_ERROR
-
-const auto buttonMacAddress = NimBLEAddress(DEVICE_MAC, 1);
+#define SCAN_INTERVAL       500 // in ms
+#define SCAN_WINDOW         450 // in ms
+#define DEVICE_NAME         "u701"
+#define SERIAL_BAUD_RATE    115200
+#define DEVICE_MANUFACTURER "u701"
+#define DEVICE_BATTERY      100
+#define ServerName          "u701"
 
 BleKeyboard keyboard(DEVICE_NAME, DEVICE_MANUFACTURER, DEVICE_BATTERY);
 
-void setupWatchdog(int timeout) {
-  Log.noticeln("Setting up watchdog @ %d ...", timeout);
-  esp_task_wdt_init(timeout, true);
-  esp_task_wdt_add(nullptr);
-}
+// A8:42:E3:CD:FB:C6, f7:97:ac:1f:f8:c0
+NimBLEAddress ServerAddress = 0xA842E3CD0C6;
 
-void restart(const char *reason) {
-  Log.noticeln(reason);
-  Log.noticeln("Restarting ESP32 in 5 seconds ...");
-  delay(5000);
-  ESP.restart();
-}
+void scanEndedCB(NimBLEScanResults results);
 
-class ClientCallback : public NimBLEClientCallbacks {
-  void onConnect(NimBLEClient *client) override {
-    esp_task_wdt_reset();
-    Log.noticeln("Connected to device!");
-  }
+// UUID HID
+static NimBLEUUID serviceUUID("1812");
+static NimBLEUUID charUUID("2a4d");
 
-  void onDisconnect(NimBLEClient *client) override {
-    restart("Disconnected from device");
-  }
+class ClientCallbacks : public NimBLEClientCallbacks {
+  void onConnect(NimBLEClient *pClient) {
+    printf("Connected, will update conn params\n");
+    pClient->updateConnParams(120, 120, 0, 1);
+  };
+
+  void onDisconnect(NimBLEClient *pClient) {
+    printf("Disconnected\n");
+    ESP.restart();
+  };
+
+  /** Called when the peripheral requests a change to the connection parameters.
+        Return true to accept and apply them or false to reject and keep
+        the currently used parameters. Default will return true.
+  */
+  bool onConnParamsUpdateRequest(NimBLEClient *pClient, const ble_gap_upd_params *params) {
+    printf("Connection parameter update request: ");
+
+    if (params->itvl_min < 24) { /** 1.25ms units */
+      return false;
+    } else if (params->itvl_max > 40) { /** 1.25ms units */
+      return false;
+    } else if (params->latency > 2) { /** Number of intervals allowed to skip */
+      return false;
+    } else if (params->supervision_timeout > 100) { /** 10ms units */
+      return false;
+    }
+
+    return true;
+  };
+
+  /** Pairing proces\s complete, we can check the results in ble_gap_conn_desc */
+  void onAuthenticationComplete(ble_gap_conn_desc *desc) {
+    if (desc->sec_state.encrypted) return;
+    printf("Encrypt connection failed - disconnecting");
+    NimBLEDevice::getClientByID(desc->conn_handle)->disconnect();
+  };
 };
+
+static ClientCallbacks clientCB;
+static NimBLEAdvertisedDevice *advDevice;
+
+/** Define a class to handle the callbacks when advertisments are received */
+class AdvertisedDeviceCallbacks : public NimBLEAdvertisedDeviceCallbacks {
+  void onResult(NimBLEAdvertisedDevice *advertisedDevice) {
+
+    if (!advertisedDevice->isAdvertisingService(serviceUUID)) {
+      printf("Not Our Service: %s\n", advertisedDevice->toString().c_str());
+      return;
+    }
+
+    if (!advertisedDevice->getAddress().equals(ServerAddress)) {
+      printf("Not Our Server: %s\n", advertisedDevice->toString().c_str());
+      return;
+    }
+
+    printf("Found Our Service: %s\n", advertisedDevice->toString().c_str());
+
+    advDevice->getScan()->stop();
+    advDevice = advertisedDevice;
+  };
+};
+
+/* Event received from the Terrain Command */
+static void onEvent(BLERemoteCharacteristic *_, uint8_t *data, size_t length, bool isNotify) {
+  if (!isNotify) {
+    return;
+  }
+
+  c_on_event(data, length);
+}
+
+extern "C" void init_arduino() {
+  printf("Starting NimBLE Client\n");
+
+  // NimBLEDevice::init("");
+  // NimBLEDevice::setSecurityAuth(BLE_SM_PAIR_AUTHREQ_BOND);
+  // NimBLEDevice::setPower(ESP_PWR_LVL_P9);
+
+  printf("Starting BLE scan\n");
+
+  auto pScan = NimBLEDevice::getScan();
+  pScan->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks());
+  pScan->setActiveScan(true);
+  pScan->setInterval(97);
+  pScan->setWindow(37);
+  pScan->start(0);
+
+  // printf("Starting keyboard\n");
+  // keyboard.begin();
+
+  if (!advDevice) {
+    printf("No advertised device to connect to\n");
+    printf("Will restart the ESP\n");
+    ESP.restart();
+  }
+
+  auto pClient = NimBLEDevice::createClient();
+  pClient->setClientCallbacks(&clientCB, false);
+  pClient->setConnectionParams(12, 12, 0, 51);
+  pClient->setConnectTimeout(5);
+
+  if (!pClient->connect(advDevice)) {
+    printf("Failed to connect, restarting ESP (1)");
+    printf("Will restart the ESP\n");
+    ESP.restart();
+  }
+
+  if (!pClient->isConnected()) {
+    printf("Failed to connect, restarting ESP (2)");
+    printf("Will restart the ESP\n");
+    ESP.restart();
+  }
+
+  printf("Connected to: %s\n", pClient->getPeerAddress().toString().c_str());
+
+  auto pSvc = pClient->getService(serviceUUID);
+  if (!pSvc) {
+    printf("Failed to find our service UUID: %s\n", serviceUUID.toString().c_str());
+    printf("Will logout the device\n");
+    pClient->disconnect();
+    printf("Will restart the ESP\n");
+    ESP.restart();
+  }
+
+  auto pChrs = pSvc->getCharacteristics(true);
+  if (!pChrs) {
+    printf("Failed to find our characteristic UUID: %s\n", charUUID.toString().c_str());
+    printf("Will logout the device\n");
+    pClient->disconnect();
+    printf("Will restart the ESP\n");
+    ESP.restart();
+  }
+
+  for (int i = 0; i < pChrs->size(); i++) {
+    if (!pChrs->at(i)->canNotify()) {
+      printf("Characteristic cannot notify\n");
+      continue;
+    }
+
+    if (!pChrs->at(i)->getUUID().equals(charUUID)) {
+      printf("Found characteristic UUID: %s\n", pChrs->at(i)->getUUID().toString().c_str());
+      continue;
+    }
+
+    if (!pChrs->at(i)->registerForNotify(onEvent)) {
+      printf("Failed to register for notify\n");
+      continue;
+    }
+
+    if (!pChrs->at(i)->subscribe(true, onEvent, false)) {
+      printf("Failed to subscribe for notify\n");
+      printf("Will logout the device\n");
+      pClient->disconnect();
+      printf("Will restart the ESP\n");
+      ESP.restart();
+    }
+  }
+}
 
 extern "C" void ble_keyboard_write(uint8_t c[2]) {
   if (keyboard.isConnected()) {
@@ -70,144 +199,4 @@ extern "C" void ble_keyboard_print(const uint8_t *format) {
 
 extern "C" bool ble_keyboard_is_connected() {
   return keyboard.isConnected();
-}
-
-/* Add function isActive to the State struct */
-static void handleButtonClick(BLERemoteCharacteristic *_, uint8_t *data, size_t length, bool isNotify) {
-  if (!isNotify) {
-    return;
-  }
-
-  c_on_event(data, length);
-}
-
-void initializeKeyboard() {
-  Log.noticeln("Enable Keyboard");
-
-  keyboard.setBatteryLevel(100);
-  keyboard.setDelay(12);
-  keyboard.begin();
-
-  Log.noticeln("Waiting for iPhone to connect to virtual keyboard ...");
-  Log.noticeln("Entering wait loop ...");
-  while (!keyboard.isConnected()) {
-    esp_task_wdt_reset();
-    delay(10);
-  }
-
-  Log.noticeln("iPhone connected to virtual keyboard");
-}
-
-void initializeSerialCommunication() {
-  Serial.begin(SERIAL_BAUD_RATE);
-  Log.begin(LOG_LEVEL_VERBOSE, &Serial);
-  Log.setLevel(LOG_LEVEL_VERBOSE);
-  Log.noticeln("Starting ESP32 ...");
-  esp_task_wdt_reset();
-}
-
-/**
- * Sets up the client to connect to the BLE device with the specified MAC address.
- * If the connection fails or no services/characteristics are found, the device will restart.
- */
-void connectToClientDevice() {
-  Log.noticeln("[Connecting] to Terrain Command ...");
-
-  if (client == nullptr) {
-    restart("Device not found, will reboot");
-  }
-
-  client->setClientCallbacks(&clientCallbackInstance);
-  if (client->isConnected()) {
-    return Log.noticeln("Already connected to device");
-  }
-
-  if (!client->connect()) {
-    restart("Could not connect to the Terrain Command");
-  }
-  // Ensure MTU has been defined by both parties
-  delay(1000);
-
-  Log.noticeln("Discovering services ...");
-  for (auto &service: *client->getServices(true)) {
-    Log.noticeln("Discovering characteristics ...");
-    for (auto &characteristic: *service->getCharacteristics(true)) {
-      auto currentServiceUUID = service->getUUID().toString().c_str();
-      auto currentCharUUID    = characteristic->getUUID().toString().c_str();
-
-      esp_task_wdt_reset();
-
-      // Register for click events
-      if (!service->getUUID().equals(hidService)) {
-        Log.warningln("[Click] Unknown report service: %X", currentServiceUUID);
-      } else if (!characteristic->getUUID().equals(reportUUID)) {
-        Log.warningln("[Click] Unknown report characteristic: %X", currentCharUUID);
-      } else if (!characteristic->canNotify()) {
-        Log.warningln("[Click] Cannot subscribe to notifications: %X", currentCharUUID);
-      } else if (!characteristic->subscribe(true, handleButtonClick, false)) {
-        Log.errorln("[Click] [Bug] Failed to subscribe to notifications: %X", currentCharUUID);
-      } else {
-        return Log.noticeln("[Click] Subscribed to notifications: %X", currentCharUUID);
-      }
-    }
-  }
-
-  Log.noticeln("Could not subscribe to notifications");
-}
-
-class Callbacks : public NimBLEAdvertisedDeviceCallbacks {
-  void onResult(NimBLEAdvertisedDevice *advertised) {
-    auto macAddr = advertised->getAddress();
-
-    esp_task_wdt_reset();
-    Serial.print(".");
-
-    if (macAddr != buttonMacAddress) {
-      return;
-    }
-
-    client = NimBLEDevice::createClient(macAddr);
-    advertised->getScan()->stop();
-
-    Log.noticeln("[SCAN] Terrain Command found");
-  }
-};
-
-void disableWatchdog() {
-  Log.noticeln("Disabling watchdog ...");
-  esp_task_wdt_delete(nullptr);
-}
-
-/**
- * Sets up the BLE scan to search for the device with the specified MAC address.
- * If the device is found, the scan will stop and the client will be set up.
- * The scan interval is set high to save power
- */
-void startBLEScanForDevice() {
-  Log.noticeln("Starting BLE scan ...");
-
-  // If frozen, the scan will not start
-  setupWatchdog(5 * 60);
-
-  auto scan = NimBLEDevice::getScan();
-  static Callbacks scanCallbackInstance;
-  scan->setAdvertisedDeviceCallbacks(&scanCallbackInstance);
-  scan->setInterval(SCAN_INTERVAL);
-  scan->setWindow(SCAN_WINDOW);
-  scan->setActiveScan(true);
-  scan->start(0, false);
-
-  Log.noticeln("BLE scan finished");
-  disableWatchdog();
-}
-
-extern "C" void init_arduino() {
-  setupWatchdog(WDT_TIMEOUT);
-  initializeKeyboard();
-  initializeSerialCommunication();
-  disableWatchdog();
-  startBLEScanForDevice();
-  setupWatchdog(WDT_TIMEOUT);
-  connectToClientDevice();
-  disableWatchdog();
 }
