415212954bbb6e96be27ce3e5929c1193960e92e
Linus Oleander <linus@oleander.io>
2023-12-25 05:18:27 +0100

    Update DEVICE_MAC in settings.h and enable info level bootloader logging in sdkconfig.release

--- include/settings.h
+++ include/settings.h
@@ -1,5 +1,5 @@
-// #define DEVICE_MAC "f7:97:ac:1f:f8:c0"
-#define DEVICE_MAC          "A8:42:E3:CD:FB:C6"
+#define DEVICE_MAC "f7:97:ac:1f:f8:c0"
+// #define DEVICE_MAC          "A8:42:E3:CD:FB:C6"
 #define DEVICE_MANUFACTURER "Oleander"
 #define DEVICE_NAME         "u701"
 #define DEVICE_BATTERY      100
