fa500132179ca1ab5ccb2ab3bc826ff188d0a979
Linus Oleander <linus@oleander.io>
2024-01-03 02:36:42 +0100

    Change log level from INFO to VERBOSE in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -147,8 +147,8 @@ extern "C" void init_arduino() {
   initArduino();
 
   Serial.begin(SERIAL_BAUD_RATE);
-  Log.begin(LOG_LEVEL_INFO, &Serial, true);
-  Log.setPrefix(printPrefix);
+  Log.begin(LOG_LEVEL_VERBOSE, &Serial, true);
+  // Log.setPrefix(printPrefix);
   Log.infoln("Starting ESP32 Proxy");
 
   NimBLEDevice::setSecurityAuth(BLE_SM_PAIR_AUTHREQ_BOND);
