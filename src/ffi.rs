eb7e10baaff1003e576ad023a87869e8c8ed914d
Linus Oleander <linus@oleander.io>
2024-01-03 18:08:28 +0100

    Update dependencies in Cargo.toml and refactor c_on_event in ffi.rs

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,114 +1,114 @@
-use esp32_nimble::{enums::{SecurityIOCap, PowerType, PowerLevel, AuthReq}, BLEDevice, BLEClient};
-use esp32_nimble::utilities::BleUuid;
-use esp32_nimble::utilities::BleUuid::Uuid16;
-use log::{error, info, debug};
-use crate::Keyboard;
-use std::sync::Arc;
-use bstr::ByteSlice;
-use tokio::sync::Notify;
+// use esp32_nimble::{enums::{SecurityIOCap, PowerType, PowerLevel, AuthReq}, BLEDevice, BLEClient};
+// use esp32_nimble::utilities::BleUuid;
+// use esp32_nimble::utilities::BleUuid::Uuid16;
+// use log::{error, info, debug};
+// use crate::Keyboard;
+// use std::sync::Arc;
+// use bstr::ByteSlice;
+// use tokio::sync::Notify;
 
-#[no_mangle]
-pub unsafe extern "C" fn c_on_event(event: *const u8, len: usize) {
-  crate::on_event(std::slice::from_raw_parts(event, len).try_into().ok());
-}
+// #[no_mangle]
+// pub unsafe extern "C" fn c_on_event(event: *const u8, len: usize) {
+//   crate::on_event(std::slice::from_raw_parts(event, len).try_into().ok());
+// }
 
-extern "C" {
-  pub fn setup_arduino();
-  pub fn setup_ble();
-  pub fn setup_app();
-}
+// extern "C" {
+//   pub fn setup_arduino();
+//   pub fn setup_ble();
+//   pub fn setup_app();
+// }
 
-const SERVICE_UUID: BleUuid = Uuid16(0x1812);
-const CHAR_UUID: BleUuid = Uuid16(0x2A4D);
+// const SERVICE_UUID: BleUuid = Uuid16(0x1812);
+// const CHAR_UUID: BleUuid = Uuid16(0x2A4D);
 
-#[no_mangle]
-#[tokio::main(flavor = "current_thread")]
-pub async extern "C" fn app_main() -> i32 {
-  env_logger::builder().filter(None, log::LevelFilter::Info).init();
+// #[no_mangle]
+// #[tokio::main(flavor = "current_thread")]
+// pub async extern "C" fn app_main() -> i32 {
+//   env_logger::builder().filter(None, log::LevelFilter::Info).init();
 
-  unsafe {
-    setup_arduino();
-    // setup_ble();
-  }
+//   unsafe {
+//     setup_arduino();
+//     // setup_ble();
+//   }
 
-  info!("Setup keyboard");
-  let mut keyboard = Keyboard::new();
+//   info!("Setup keyboard");
+//   let mut keyboard = Keyboard::new();
 
-  let notify = Arc::new(Notify::new());
-  let notify_clone = notify.clone();
+//   let notify = Arc::new(Notify::new());
+//   let notify_clone = notify.clone();
 
-  keyboard.on_authentication_complete(move |conn| {
-    info!("Connected to {:?}", conn);
-    info!("Notifying notify");
-    notify_clone.notify_one();
-  });
+//   keyboard.on_authentication_complete(move |conn| {
+//     info!("Connected to {:?}", conn);
+//     info!("Notifying notify");
+//     notify_clone.notify_one();
+//   });
 
-  info!("Waiting for notify to be notified");
-  notify.notified().await;
+//   info!("Waiting for notify to be notified");
+//   notify.notified().await;
 
-  let device = BLEDevice::take();
-  device
-    .set_power(PowerType::Default, PowerLevel::P9)
-    .unwrap();
-  device
-    .security()
-    .set_auth(AuthReq::Bond)
-    .set_io_cap(SecurityIOCap::NoInputNoOutput);
+//   let device = BLEDevice::take();
+//   device
+//     .set_power(PowerType::Default, PowerLevel::P9)
+//     .unwrap();
+//   device
+//     .security()
+//     .set_auth(AuthReq::Bond)
+//     .set_io_cap(SecurityIOCap::NoInputNoOutput);
 
-  let ble_scan = device.get_scan();
+//   let ble_scan = device.get_scan();
 
-  let device = ble_scan
-    .active_scan(true)
-    .interval(100)
-    .window(99)
-    .find_device(i32::MAX, move |device| {
-      device.name().contains_str("key") || device.name().contains_str("Terrain")
-    })
-    .await
-    .unwrap();
+//   let device = ble_scan
+//     .active_scan(true)
+//     .interval(100)
+//     .window(99)
+//     .find_device(i32::MAX, move |device| {
+//       device.name().contains_str("key") || device.name().contains_str("Terrain")
+//     })
+//     .await
+//     .unwrap();
 
-  let Some(device) = device else {
-    ::log::warn!("device not found");
-    return 1;
-  };
+//   let Some(device) = device else {
+//     ::log::warn!("device not found");
+//     return 1;
+//   };
 
-  let mut client = BLEClient::new();
-  client.connect(device.addr()).await.unwrap();
-  client.secure_connection().await.unwrap();
+//   let mut client = BLEClient::new();
+//   client.connect(device.addr()).await.unwrap();
+//   client.secure_connection().await.unwrap();
 
-  info!("Connected to {:?}", device);
+//   info!("Connected to {:?}", device);
 
-  debug!("[characteristic] finding service ...");
-  let mut characteristic = client
-    .get_service(SERVICE_UUID)
-    .await
-    .unwrap()
-    .get_characteristics()
-    .await
-    .unwrap()
-    .find(|x| x.uuid() == CHAR_UUID && x.can_notify())
-    .unwrap()
-    .to_owned();
+//   debug!("[characteristic] finding service ...");
+//   let mut characteristic = client
+//     .get_service(SERVICE_UUID)
+//     .await
+//     .unwrap()
+//     .get_characteristics()
+//     .await
+//     .unwrap()
+//     .find(|x| x.uuid() == CHAR_UUID && x.can_notify())
+//     .unwrap()
+//     .to_owned();
 
-  debug!("[characteristic] subscribing to notifications ...");
-  characteristic.on_notify(move |data| {
-    info!("[on_notify] data={:?}", data);
+//   debug!("[characteristic] subscribing to notifications ...");
+//   characteristic.on_notify(move |data| {
+//     info!("[on_notify] data={:?}", data);
 
-    if let [0, 0, id, _] = data {
-      crate::on_event(Some(&[0, 0, *id, 0]));
-    } else {
-      error!("[on_notify] invalid data: {:?}", data);
-    }
-  });
+//     if let [0, 0, id, _] = data {
+//       crate::on_event(Some(&[0, 0, *id, 0]));
+//     } else {
+//       error!("[on_notify] invalid data: {:?}", data);
+//     }
+//   });
 
-  if let Err(e) = characteristic.subscribe_notify(false).await {
-    error!("[characteristic] could not subscribe to notifications: {:?}", e);
-  }
+//   if let Err(e) = characteristic.subscribe_notify(false).await {
+//     error!("[characteristic] could not subscribe to notifications: {:?}", e);
+//   }
 
 
-  if let Err(e) = crate::runtime(keyboard).await {
-    error!("[error] Error: {:?}", e);
-  }
+//   if let Err(e) = crate::runtime(keyboard).await {
+//     error!("[error] Error: {:?}", e);
+//   }
 
-  return 0;
-}
+//   return 0;
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,121 @@
+// mod keyboard;
+// mod ffi;
+
+// use std::sync::mpsc::{channel, Receiver, Sender};
+// use log::{debug, info, error};
+// use lazy_static::lazy_static;
+// use anyhow::{bail, Result};
+// use keyboard::Keyboard;
+// use std::sync::Mutex;
+// use machine::Action;
+
+// lazy_static! {
+//   static ref CHANNEL: (Sender<u8>, Mutex<Receiver<u8>>) = {
+//     let (send, recv) = channel();
+//     let recv = Mutex::new(recv);
+//     (send, recv)
+//   };
+// }
+
+// async fn runtime(mut keyboard: Keyboard) -> Result<()> {
+//   info!("[main] Starting main loop");
+
+//   let mut state = machine::State::default();
+
+//   let receiver = CHANNEL.1.lock().unwrap();
+//   info!("[main] Entering loop, waiting for events");
+//   while let Ok(event_id) = receiver.recv() {
+//     match state.transition(event_id) {
+//       Some(Action::Media(_keys)) => keyboard.write("M"),
+//       Some(Action::Short(_index)) => keyboard.write("S"),
+//       None => debug!("[main] No action {}", event_id)
+//     }
+//   }
+
+//   bail!("[main] Event loop ended");
+// }
+
+// pub fn on_event(event: Option<&[u8; 4]>) {
+//   match event {
+//     Some(&[_, _, 0, _]) => debug!("Button was released"),
+//     Some(&[_, _, n, _]) => CHANNEL.0.send(n).unwrap(),
+//     None => error!("[on_event] [BUG] Received {:?} event", event)
+//   }
+// }
+
+#![no_main]
+#![feature(never_type)]
+#![allow(unused_imports)]
+
+extern crate esp_idf_hal;
+extern crate lazy_static;
+extern crate log;
+
 mod keyboard;
-mod ffi;
 
-use std::sync::mpsc::{channel, Receiver, Sender};
-use log::{debug, info, error};
-use lazy_static::lazy_static;
-use anyhow::{bail, Result};
+use embassy_time::{Duration, Timer};
+use esp32_nimble::utilities::mutex::Mutex;
+use esp32_nimble::utilities::BleUuid;
+use esp32_nimble::utilities::BleUuid::Uuid16;
+use esp32_nimble::{BLEClient, BLEDevice};
+use esp_idf_hal::task::block_on;
 use keyboard::Keyboard;
-use std::sync::Mutex;
-use machine::Action;
+use log::{info, warn};
 
-lazy_static! {
-  static ref CHANNEL: (Sender<u8>, Mutex<Receiver<u8>>) = {
-    let (send, recv) = channel();
-    let recv = Mutex::new(recv);
-    (send, recv)
-  };
-}
+const SERVICE_UUID: BleUuid = Uuid16(0x1812);
+const CHAR_UUID: BleUuid = Uuid16(0x2A4D);
+const BLE_BUTTONS_NAME: &str = "tob";
 
-async fn runtime(mut keyboard: Keyboard) -> Result<()> {
-  info!("[main] Starting main loop");
+#[no_mangle]
+fn app_main() {
+  esp_idf_sys::link_patches();
+  esp_idf_svc::log::EspLogger::initialize_default();
+  esp_idf_svc::timer::embassy_time::driver::link();
 
-  let mut state = machine::State::default();
+  info!("Setup connection to BLE buttons");
+  block_on(async {
+    let keyboard = Mutex::new(Keyboard::new());
 
+    let device = BLEDevice::take();
+    let scan = device.get_scan();
 
-  let receiver = CHANNEL.1.lock().unwrap();
-  info!("[main] Entering loop, waiting for events");
-  while let Ok(event_id) = receiver.recv() {
-    match state.transition(event_id) {
-      Some(Action::Media(_keys)) => keyboard.write("M"),
-      Some(Action::Short(_index)) => keyboard.write("S"),
-      None => debug!("[main] No action {}", event_id)
+    let device = scan
+      .active_scan(true)
+      .interval(490)
+      .window(450)
+      .find_device(i32::MAX, |device| device.is_advertising_service(&SERVICE_UUID))
+      .await
+      .expect("Failed to find device")
+      .expect("No device found");
+
+    let mut client = BLEClient::new();
+
+    // client.on_passkey_request(callback)
+    client.on_connect(move |_thing| {
+      info!("Connected to device");
+    });
+
+    client.connect(device.addr()).await.expect("Failed to connect to device");
+
+    let service = client.get_service(SERVICE_UUID).await.expect("Failed to get service");
+    let characteristic = service.get_characteristic(CHAR_UUID).await.expect("Failed to get characteristic");
+
+    let status = characteristic
+      .on_notify(move |_data| {
+        info!("Received notification from device");
+        let mut keyboard = keyboard.lock();
+        if keyboard.connected() {
+          keyboard.write("Hello world");
+        }
+      })
+      .subscribe_notify(false)
+      .await
+      .ok();
+
+    info!("Waiting for notifications: {:?}", status);
+
+    loop {
+      Timer::after(Duration::from_secs(1)).await;
     }
-  }
-
-  bail!("[main] Event loop ended");
-}
-
-pub fn on_event(event: Option<&[u8; 4]>) {
-  match event {
-    Some(&[_, _, 0, _]) => debug!("Button was released"),
-    Some(&[_, _, n, _]) => CHANNEL.0.send(n).unwrap(),
-    None => error!("[on_event] [BUG] Received {:?} event", event)
-  }
+  });
 }
