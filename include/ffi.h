19699ce39b552a335b83663cdee605f12e524f8a
Linus Oleander <linus@oleander.io>
2024-01-03 06:15:30 +0100

    Refactor ffi.h: Remove duplicate code and add setupBLEConnections function

--- include/ffi.h
+++ include/ffi.h
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-extern "C" void ble_keyboard_write(uint8_t c[2]);
-extern "C" void ble_keyboard_print(const uint8_t *format);
 extern "C" void c_on_event(const uint8_t *event, size_t length);
+extern "C" void ble_keyboard_print(const uint8_t *format);
+extern "C" void ble_keyboard_write(uint8_t c[2]);
+extern "C" void setupBLEConnections();
