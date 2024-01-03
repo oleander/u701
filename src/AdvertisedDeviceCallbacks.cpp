feb3dd2c159350669308180e83a27d05db681564
Linus Oleander <linus@oleander.io>
2024-01-03 05:01:19 +0100

    Fix AdvertisedDeviceCallbacks method definition

--- src/AdvertisedDeviceCallbacks.cpp
+++ src/AdvertisedDeviceCallbacks.cpp
@@ -1,13 +1,6 @@
-#ifndef ADVERTISED_DEVICE_CALLBACKS_H
-#define ADVERTISED_DEVICE_CALLBACKS_H
-
+#include "AdvertisedDeviceCallbacks.h"
 #include <NimBLEAdvertisedDevice.h>
 
-/** Define a class to handle the callbacks when advertisements are received */
-class AdvertisedDeviceCallbacks : public NimBLEAdvertisedDeviceCallbacks {
-  void onResult(NimBLEAdvertisedDevice *advertisedDevice) {
-    advertisedDevice->getScan()->stop();
-  };
-};
-
-#endif // ADVERTISED_DEVICE_CALLBACKS_H
+void AdvertisedDeviceCallbacks::onResult(NimBLEAdvertisedDevice *advertisedDevice) {
+  advertisedDevice->getScan()->stop();
+}
