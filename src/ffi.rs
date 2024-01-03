341469065d06d383e23ad9a20eaa7732321dce05
Linus Oleander <linus@oleander.io>
2024-01-03 15:41:20 +0100

    Refactor main app logic: Move keyboard and notify setup from lib.rs to ffi.rs

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,4 +1,7 @@
 use log::{error, info};
+use crate::Keyboard;
+use std::sync::Arc;
+use tokio::sync::Notify;
 
 #[no_mangle]
 pub unsafe extern "C" fn c_on_event(event: *const u8, len: usize) {
@@ -14,13 +17,28 @@ pub extern "C" fn app_main() -> i32 {
   env_logger::builder().filter(None, log::LevelFilter::Info).init();
 
   info!("[app_main] Calling setup");
+
   unsafe {
     init_arduino();
   }
 
   info!("[app_main] Entering main loop");
   tokio::spawn(async move {
-    if let Err(e) = crate::main().await {
+    let mut keyboard = Keyboard::new();
+
+    let notify = Arc::new(Notify::new());
+    let notify_clone = notify.clone();
+
+    keyboard.on_authentication_complete(move |conn| {
+      info!("Connected to {:?}", conn);
+      info!("Notifying notify");
+      notify_clone.notify_one();
+    });
+
+    info!("Waiting for notify to be notified");
+    notify.notified().await;
+
+    if let Err(e) = crate::runtime(keyboard).await {
       error!("[error] Error: {:?}", e);
     }
   });
--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,9 @@ use std::sync::mpsc::{channel, Receiver, Sender};
 use log::{debug, info, error};
 use lazy_static::lazy_static;
 use anyhow::{bail, Result};
-use tokio::sync::Notify;
 use keyboard::Keyboard;
 use std::sync::Mutex;
 use machine::Action;
-use std::sync::Arc;
 
 lazy_static! {
   static ref CHANNEL: (Sender<u8>, Mutex<Receiver<u8>>) = {
@@ -19,23 +17,11 @@ lazy_static! {
   };
 }
 
-async fn main() -> Result<()> {
+async fn runtime(mut keyboard: Keyboard) -> Result<()> {
   info!("[main] Starting main loop");
 
   let mut state = machine::State::default();
-  let mut keyboard = Keyboard::new();
 
-  let notify = Arc::new(Notify::new());
-  let notify_clone = notify.clone();
-
-  keyboard.on_authentication_complete(move |conn| {
-    info!("Connected to {:?}", conn);
-    info!("Notifying notify");
-    notify_clone.notify_one();
-  });
-
-  info!("Waiting for notify to be notified");
-  notify.notified().await;
 
   let receiver = CHANNEL.1.lock().unwrap();
   info!("[main] Entering loop, waiting for events");
