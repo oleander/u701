8fa79764dc5e1b8a3fdafeaa4c5f37a3955d9821
Linus Oleander <linus@oleander.io>
2024-01-16 00:15:25 +0100

    Replace warning log with serial print for ESP32 proxy start message

--- src/main.cpp
+++ src/main.cpp
@@ -122,7 +122,7 @@ namespace llvm_libc {
 
     Serial.begin(SERIAL_BAUD_RATE);
     Log.begin(LOG_LEVEL_INFO, &Serial, true);
-    Log.warningln("Starting ESP32 Proxy @ %s", GIT_COMMIT);
+    Serial.println("Starting ESP32 Proxy @ " + String(GIT_COMMIT));
 
     updateWatchdogTimeout(WATCHDOG_TIMEOUT_1);
     NimBLEDevice::setSecurityAuth(BLE_SM_PAIR_AUTHREQ_BOND);
