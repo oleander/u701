6f6c3917f52c83414893038b8fc14c9d4b7f2be1
Linus Oleander <linus@oleander.io>
2024-01-15 10:54:12 +0100

    Remove redundant commands from setup script

--- setup.sh
+++ setup.sh
@@ -7,7 +7,5 @@ cargo +nightly install cargo-binstall
 cargo +nightly binstall cargo-pio espup just -y
 cargo +nightly pio installpio
 
-# just clean build
-espup install -t esp32c3 -s
 source  ~/.bashrc
 cargo +esp build
