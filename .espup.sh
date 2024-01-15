cbeb66061d8283487340dea963eb5c1cc6bf1ec1
Linus Oleander <linus@oleander.io>
2024-01-15 10:02:17 +0100

    Add devcontainer.json and update .env for improved Rust dev environment

--- .espup.sh
+++ .espup.sh
@@ -1,2 +1,1 @@
-export PATH="/Users/linusoleander/.rustup/toolchains/esp/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin:$PATH"
-export LIBCLANG_PATH="/Users/linusoleander/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-16.0.4-20231113/esp-clang/lib"
+export LIBCLANG_PATH="/Users/linus/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-16.0.4-20231113/esp-clang/lib"
