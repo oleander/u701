895d777d1c385861c53869e253a0d32ae7987adb
Linus Oleander <linus@oleander.io>
2024-01-15 10:13:06 +0100

    Change setup.sh file permissions and refine installation steps

--- setup.sh
+++ setup.sh
@@ -6,6 +6,5 @@ source .env
 cargo +nightly install cargo-binstall
 cargo +nightly binstall cargo-pio espup just -y
 cargo +nightly pio installpio
-espup install -t $MCU -f .espup.sh
-source .espup.sh
+espup install -t $MCU
 cargo +esp pio build
