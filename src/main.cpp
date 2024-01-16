03b7aefdb3c208e2184b3c6d9788d2117da3c07b
Linus Oleander <linus@oleander.io>
2024-01-16 05:34:30 +0100

    "Implement ArduinoOTA setup function in main.cpp for over-the-air updates"

--- src/main.cpp
+++ src/main.cpp
@@ -117,6 +117,60 @@ namespace llvm_libc {
     return false;
   }
 
+#define SSID "boat"
+#define PASS "0304673428"
+
+  void setupArduinoOTA() {
+    Log.noticeln("Setting up OTA");
+    if (WiFi.status() == WL_NO_SHIELD) {
+      Log.errorln("WiFi shield not present");
+      return;
+    }
+
+    while (status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to SSID: ");
+      Serial.println(SSID);
+      status = WiFi.begin(SSID, PASS);
+    }
+
+    ArduinoOTA.setHostname("u701");
+    ArduinoOTA.setRebootOnSuccess(true);
+
+    ArduinoOTA.onStart([]() {
+      if (ArduinoOTA.getCommand() == U_SPIFFS) {
+        SPIFFS.end();
+      }
+    });
+
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+
+    ArduinoOTA.onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+        Serial.println("Auth Failed");
+      } else if (error == OTA_BEGIN_ERROR) {
+        Serial.println("Begin Failed");
+      } else if (error == OTA_CONNECT_ERROR) {
+        Serial.println("Connect Failed");
+      } else if (error == OTA_RECEIVE_ERROR) {
+        Serial.println("Receive Failed");
+      } else if (error == OTA_END_ERROR) {
+        Serial.println("End Failed");
+      }
+    });
+
+    ArduinoOTA.begin();
+
+    Log.noticeln("OTA ready");
+
+    while (true) {
+      ArduinoOTA.handle();
+      delay(1000);
+    }
+  }
+
   void setup() {
     initArduino();
 
